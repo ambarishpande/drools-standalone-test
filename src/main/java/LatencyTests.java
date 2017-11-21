4e0c09e63f04a0fcd816485c194fb89cf04ee2f8
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-11-21 18:33:08 +0530

    fuh

--- src/main/java/LatencyTests.java
+++ src/main/java/LatencyTests.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-
+import java.util.Map;
 
 import org.kie.api.KieServices;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
 import org.kie.api.event.rule.AfterMatchFiredEvent;
 import org.kie.api.event.rule.AgendaEventListener;
 import org.kie.api.event.rule.AgendaGroupPoppedEvent;
@@ -26,6 +29,7 @@ import org.kie.api.event.rule.RuleRuntimeEventListener;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import org.apache.commons.lang.mutable.MutableLong;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -41,12 +45,31 @@ public class LatencyTests
   public static void main(String args[])
   {
     Logger.getRootLogger().setLevel(Level.OFF);
+
     int numTransactions = Integer.parseInt(System.getProperty("n"));
     int numSessions = Integer.parseInt(System.getProperty("s"));
+//    int numTransactions = Integer.parseInt("10000");
+//    int numSessions = Integer.parseInt("10");
+
 
     Runtime runtime = Runtime.getRuntime();
     long beforeUsedMem = runtime.totalMemory() - runtime.freeMemory();
+
+    KieServices kieServices = KieServices.Factory.get();
+    KieContainer kieContainer = kieServices.newKieClasspathContainer();
     HashMap<Integer, KieSession> sessions = new HashMap<Integer, KieSession>(numSessions);
+    for(int i = 0; i< numSessions; i++){
+      final KieSession kieSession = kieContainer.newKieSession();
+      sessions.put(i,kieSession);
+      new Thread(new Runnable()
+      {
+        @Override
+        public void run()
+        {
+          kieSession.fireUntilHalt();
+        }
+      }).start();
+    }
 
     TransactionGenerator gen = getTransactionGenerator();
 
@@ -55,11 +78,7 @@ public class LatencyTests
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    KieServices kieServices = KieServices.Factory.get();
-    KieContainer kieContainer = kieServices.newKieClasspathContainer();
     KieSession kieSession = null;
-
     for (int i = 0; i < numTransactions; i++) {
       if(i%1000==0){
         try {
@@ -71,34 +90,117 @@ public class LatencyTests
       Transaction t = gen.generateTransaction(null);
       int sid = t.getCustomer().hashCode() % numSessions;
 
-      if (!sessions.containsKey(sid)) {
-        kieSession = kieContainer.newKieSession();
-        sessions.put(sid, kieSession);
-      }
       kieSession = sessions.get(sid);
-
       kieSession.insert(t);
-      kieSession.fireAllRules();
+
     }
-
     long afterUsedMem = runtime.totalMemory() - runtime.freeMemory();
-//    System.out.println(numSessions + "," + numTransactions + "," + (double)((double)(afterUsedMem - beforeUsedMem)
-//      /1000000000));
-    try {
-      PrintWriter pw = new PrintWriter(new FileOutputStream(
-        new File("log.txt"),
-        true /* append = true */));
-      pw.append(numSessions + "," + numTransactions + "," + (double)((double)(afterUsedMem - beforeUsedMem)
-        /1000000000) + "\n");
 
+    try {
+      PrintWriter pw = new PrintWriter(new FileOutputStream(new File("log.txt"),true /* append = true */));
+      pw.append(numSessions + "," + numTransactions + "," + (double)((double)(afterUsedMem - beforeUsedMem)/1000000000) + "\n");
       pw.close();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
 
-//    kieSession.dispose();
-//    kieSession.destroy();
+//    for (int i = 0; i < numSessions; i++){
+//      System.out.print("Num Facts : " + sessions.get(i).getFactCount() + " ");
+//      for (AgendaEventListener r : sessions.get(i).getAgendaEventListeners()
+//      ) {
+//          if(r instanceof RuleCountListener){
+//            System.out.println(i + " - " + ((RuleCountListener)r).getMatches());
+//         }
+//      }
+//    }
+    System.out.println("Done");
+  }
 
+
+  public static class RuleCountListener implements AgendaEventListener
+  {
+    long matches;
+    Map<String, MutableLong> ruleCount = new HashMap();
+
+    @Override
+    public void matchCreated(MatchCreatedEvent matchCreatedEvent)
+    {
+
+    }
+
+    @Override
+    public void matchCancelled(MatchCancelledEvent matchCancelledEvent)
+    {
+
+    }
+
+    @Override
+    public void beforeMatchFired(BeforeMatchFiredEvent beforeMatchFiredEvent)
+    {
+      matches++;
+      Rule rule = beforeMatchFiredEvent.getMatch().getRule();
+      if (rule != null) {
+        String name = rule.getName();
+        MutableLong count = ruleCount.get(name);
+        if (count == null) {
+          count = new MutableLong();
+          ruleCount.put(name, count);
+        }
+        count.increment();
+      }
+    }
+
+    @Override
+    public void afterMatchFired(AfterMatchFiredEvent afterMatchFiredEvent)
+    {
+
+    }
+
+    @Override
+    public void agendaGroupPopped(AgendaGroupPoppedEvent agendaGroupPoppedEvent)
+    {
+
+    }
+
+    @Override
+    public void agendaGroupPushed(AgendaGroupPushedEvent agendaGroupPushedEvent)
+    {
+
+    }
+
+    @Override
+    public void beforeRuleFlowGroupActivated(RuleFlowGroupActivatedEvent ruleFlowGroupActivatedEvent)
+    {
+
+    }
+
+    @Override
+    public void afterRuleFlowGroupActivated(RuleFlowGroupActivatedEvent ruleFlowGroupActivatedEvent)
+    {
+
+    }
+
+    @Override
+    public void beforeRuleFlowGroupDeactivated(RuleFlowGroupDeactivatedEvent ruleFlowGroupDeactivatedEvent)
+    {
+
+    }
+
+    @Override
+    public void afterRuleFlowGroupDeactivated(RuleFlowGroupDeactivatedEvent ruleFlowGroupDeactivatedEvent)
+    {
+
+    }
+
+    public long getMatches()
+    {
+      return matches;
+    }
+
+    public Map<String, MutableLong> getRuleCount()
+    {
+      return ruleCount;
+    }
   }
 
   public static TransactionGenerator getTransactionGenerator()
